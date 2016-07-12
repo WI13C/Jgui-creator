81fa872da281f8b3a0f6ef2d618648bd4f676623
Tim Bayer <Tim@Tims-MacBook-Air.local>
2016-07-12 13:15:34 +0200

    Add Basis GUIKomponente

---
+++ code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/util/GUIKomponente.java
@@ -0,0 +1,17 @@
+package de.dhbw.wi13c.jguicreator.data.util;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public abstract class GUIKomponente extends JPanel
+{
+	GUIKomponente()
+	{
+		setPreferredSize(new Dimension(200, 200));
+		setOpaque(true);
+		setBackground(Color.RED);
+		setVisible(true);
+	}
+}

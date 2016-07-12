6c338f2f4ca14f1d547a271a76796908162fa0dc
LukasHessenthaler <lukas_hessenthaler@me.com>
2016-07-12 10:32:51 +0200

    Fix BarChart, PieChart and Id-Annotation

--- code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/annotation/Id.java
+++ code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/annotation/Id.java
@@ -1,5 +1,19 @@
 package de.dhbw.wi13c.jguicreator.data.annotation;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @author lukashessenthaler
+ * Id:
+ * An annotated field will be handled like a primary-key.
+ *
+ */
+@Target({FIELD})
+@Retention(RUNTIME)
 public @interface Id {
 
 }
--- code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/annotation/PieChart.java
+++ code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/annotation/PieChart.java
@@ -1,5 +1,19 @@
 package de.dhbw.wi13c.jguicreator.data.annotation;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @author lukashessenthaler
+ * PieChart:
+ * An annotated map-field will be displayed as a pie-chart.
+ *
+ */
+@Target({FIELD})
+@Retention(RUNTIME)
 public @interface PieChart {
 
 }

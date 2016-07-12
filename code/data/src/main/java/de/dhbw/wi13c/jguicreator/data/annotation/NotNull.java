95b24f7c2feefc49bc19ed4dce724ad391c17522
LukasHessenthaler <lukas_hessenthaler@me.com>
2016-07-12 09:57:29 +0200

    Fix NotNull-Annotation

--- code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/annotation/NotNull.java
+++ code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/annotation/NotNull.java
@@ -1,5 +1,18 @@
 package de.dhbw.wi13c.jguicreator.data.annotation;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/*
+ * Size-Annotation:
+ * Set field as a not nullable field.
+ */
+@Target({FIELD})
+@Retention(RUNTIME)
 public @interface NotNull {
+	String message() default "Field cannot be empty.";
 
 }

a379e60f953cc29e62ffde1193d463940c1a1af3
Lukas Hessenthaler <lukashessenthaler@lukass-mbp.speedport_w_724v_09011603_00_023>
2016-08-22 00:43:26 +0200

    ADD Starter-Project to source control

---
+++ jgui-creator-starter/src/de/dhbw/vs/jprakt/exam/testclasses/Showcase.java
@@ -0,0 +1,60 @@
+package de.dhbw.vs.jprakt.exam.testclasses;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.dhbw.wi13c.jguicreator.DomainObjectParser;
+import de.dhbw.wi13c.jguicreator.impl.GuiCreator;
+import de.dhbw.wi13c.jguicreator.listener.SavedListener;
+
+public class Showcase {
+
+	public static void main(String[] args) {
+
+		Adresse adresse = new Adresse("Hauptstra�e", 30);
+		Kontakt privat = new Kontakt("privat", "0160123456", "ferdi@prov.de");
+		Kontakt business = new Kontakt("business", "0173555666", "ferdi@busy.com");
+
+		List<Kontakt> kontakte = new ArrayList<Kontakt>();
+		kontakte.add(privat);
+		kontakte.add(business);
+
+		Map<String, Integer> einkommensZusammensetzung = new HashMap<String, Integer>();
+		einkommensZusammensetzung.put("Miete", 20000);
+		einkommensZusammensetzung.put("Gehalt", 35000);
+		einkommensZusammensetzung.put("Zinsen", 15000);
+
+		Map<String, Double> einkommensEntwicklung = new HashMap<String, Double>();
+		einkommensEntwicklung.put("2010", 27323.54);
+		einkommensEntwicklung.put("2011", 29263.12);
+		einkommensEntwicklung.put("2012", 33131.64);
+		einkommensEntwicklung.put("2013", 34123.12);
+
+		Person aPerson = new Person("Hans", "Maier", new Integer(3), new Date(1980, 3, 24), adresse, kontakte,
+				einkommensZusammensetzung, einkommensEntwicklung);
+
+		// Dear students,
+		// this shut work in your Framework, while the class "GuiGenerator" is a
+		// class of you
+		// GuiGenerator.createView(aPerson, new PersonGespeichertListener() {
+		// @Override
+		// public void gespeichert(Person changedPerson) {
+		// // ...changedPerson ist das in ihren Masken ver�nderte Person
+		// }
+		// });
+
+		System.out.println(aPerson.toString());
+
+		GuiCreator.createView(aPerson, new SavedListener<Person>() {
+			public void saved(Person object) {
+				System.out.println(object.toString());
+
+			}
+		}, new DomainObjectParser());
+
+		// good luck
+	}
+}

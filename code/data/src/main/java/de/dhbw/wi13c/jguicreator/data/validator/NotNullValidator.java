8cb9debb64ba0d833d54842295740ebcbb7bd26b
Lukas Hessenthaler <lukashessenthaler@anna-5s.speedport.ip>
2016-08-27 09:49:45 +0200

    FIX Refactoring part 2

--- code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/validator/NotNullValidator.java
+++ code/data/src/main/java/de/dhbw/wi13c/jguicreator/data/validator/NotNullValidator.java
@@ -3,12 +3,17 @@ package de.dhbw.wi13c.jguicreator.data.validator;
 import de.dhbw.wi13c.jguicreator.data.annotation.AnnotationMessage;
 import de.dhbw.wi13c.jguicreator.data.uielements.UiElementData;
 
+/**
+ * Checks if the given element is noll or not
+ * @author Tobias Lang
+ *
+ */
 public class NotNullValidator extends Validator<Object>
 {
 
-	UiElementData element;
+	UiElementData<?> element;
 
-	public NotNullValidator(UiElementData element)
+	public NotNullValidator(UiElementData<?> element)
 	{
 
 		this.element = element;
